456ae4fd079bddb2f7a58bd47fda5fde8081c407
hungkgvl <hungkgvl@gmail.com>
2026-04-04 12:29:39 +0700

    Fix

--- GateWay/GateWay/Models/Auth/RegisterRequest.cs
+++ GateWay/GateWay/Models/Auth/RegisterRequest.cs
@@ -1,7 +1,6 @@
 ﻿public class RegisterRequest
 {
-    public string? FullName { get; set; }
-    public string? PhoneNumber { get; set; }
-    public string? Bio { get; set; }
-    public string? AvatarUrl { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
 }
